f1194bb21fafdc6c432796acb772057694ec43d6
Sheler <ondraglaser@gmail.com>
2018-02-18 22:16:48 +0100

    Initial commit

---
+++ app/src/main/java/com/glaserproject/flicks/NetUtils/MovieDbUtils.java
@@ -0,0 +1,11 @@
+package com.glaserproject.flicks.NetUtils;
+
+/**
+ * Created by ondra on 2/18/2018.
+ */
+
+public class MovieDbUtils {
+
+    private final static String MOVIE_DB_API_KEY = "e55aedf14cf9d30387a9a34d7d511bdc";
+
+}
