27e4d56ab7dbfc163cd93b3172b3c4cd1e4ad2e1
Sheler <ondraglaser@gmail.com>
2018-03-24 12:03:35 +0100

    add Toast

--- app/src/main/java/com/glaserproject/flicks/DetailActivity.java
+++ app/src/main/java/com/glaserproject/flicks/DetailActivity.java
@@ -16,6 +16,7 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.glaserproject.flicks.Favorites.FavoritesContract;
 import com.glaserproject.flicks.MyObjects.Movie;
